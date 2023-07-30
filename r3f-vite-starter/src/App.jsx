d865eee2a1e85a9ba835302a826e01a5270c385a
luc.nguyen <luc.nguyen@epita.fr>
2023-07-30 08:14:37 +0300

    feat: Menu

--- r3f-vite-starter/src/App.jsx
+++ r3f-vite-starter/src/App.jsx
@@ -1,11 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-import { ScrollControls,Scroll } from "@react-three/drei";
+import { ScrollControls, Scroll } from "@react-three/drei";
 import { Interface } from "./components/Interface";
 import { ScrollManager } from "./components/ScrollManager";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { Menu } from "./components/Menu";
 function App() {
   const [section, setSection] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  useEffect(() => {
+    setMenuOpened(false);
+  }, [section]);
   return (
     <>
       <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
@@ -14,10 +20,13 @@ function App() {
           <ScrollManager section={section} onSectionChange={setSection} />
           <Experience />
           <Scroll html>
-           <Interface/>
+            <Interface />
           </Scroll>
         </ScrollControls>
       </Canvas>
+      <Menu onSectionChange={setSection}
+       menuOpened={menuOpened}
+        setMenuOpened={setMenuOpened} />
 
     </>
 
