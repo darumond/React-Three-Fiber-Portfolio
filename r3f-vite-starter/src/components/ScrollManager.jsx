7355774494cae47c2fd38f21c18acf9995336ae6
luc.nguyen <luc.nguyen@epita.fr>
2023-07-31 19:16:27 +0300

    fix: scrolling

--- r3f-vite-starter/src/components/ScrollManager.jsx
+++ r3f-vite-starter/src/components/ScrollManager.jsx
@@ -26,24 +26,23 @@ export const ScrollManager = (props) => {
     });
   }, [section]);
 
-  useFrame(() => {
-    if (isAnimating.current) {
-      lastScroll.current = data.scroll.current;
-      return;
-    }
-
-    const curSection = Math.floor(data.scroll.current * data.pages);
-    if (data.scroll.current > lastScroll.current && curSection === 0) {
-      onSectionChange(1);
-    }
-    if (
-      data.scroll.current < lastScroll.current &&
-      data.scroll.current < 1 / (data.pages - 1)
-    ) {
-      onSectionChange(0);
-    }
+useFrame(() => {
+  if (isAnimating.current) {
     lastScroll.current = data.scroll.current;
-  });
+    return;
+  }
+
+  const curSection = Math.floor(data.scroll.current * data.pages);
+  if (data.scroll.current > lastScroll.current && curSection === 0) {
+    onSectionChange(1);
+  }
+  console.log(data.scroll.current <= 1 / (data.pages - 1))
+  if (data.scroll.current < lastScroll.current && lastScroll.current <= 1 / (data.pages - 1)) {
+    onSectionChange(0);
+  }
+  lastScroll.current = data.scroll.current;
+});
+
 
   return null;
 };
